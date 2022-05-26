573de8bf6ee45ee957b6c2e37b29c96e60a4b92c
Mateosanzep <mateosanzep@gmail.com>
2022-05-25 23:32:03 -0500

    commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+//tipo de datos:
+document.write("<h1>hola mundo</h1>");
+console.log("hola");
+console.error("hola");
+console.warn("error");
+console.info("info");
+console.table([a, b, c]); // Se hace una tabla con los valores
+console.table({a, b, c});
+
+let a = 10;
+var b = 11; // este no se usa
+const c = 13;
+
+console.log(a + c);
+console.log({ a }); // Poner los valores entre llaves hace que en la consola nos salga el nombre de la variable
+alert('estas seguro de esto') // Pone como una notificacion
+j 
